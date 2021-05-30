23b8ce38df887f99657ff0ac3b891f4cad4181f9
argos66 <argos66@users.noreply.github.com>
2021-05-30 09:37:05 +0200

    Update PZEMv30_ESP32HwSerial_Wifi.ino.ino

--- PZEMv30_ESP32HwSerial_Wifi.ino.ino
+++ PZEMv30_ESP32HwSerial_Wifi.ino.ino
@@ -1,74 +1,126 @@
 #include <HardwareSerial.h>
 #include <PZEM004Tv30.h>
 #include <WiFi.h>
+#include <WebServer.h>
+#include "time.h"
 
-#define wait 1000
+#define wait 2
 
 const char* ssid     = ""; //Wifi SSID
 const char* password = ""; //Wifi password
 
-WiFiServer server(80); //Listen on 80
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 3600;
+const int   daylightOffset_sec = 3600;
+
+WebServer server(80); //Listen on 80
 HardwareSerial PzemSerial2(2); // Use hwserial UART2 at pins IO-16 (RX2) and IO-17 (TX2)
 PZEM004Tv30 pzem(&PzemSerial2);
 
+void printLocalTime(){
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "NTP Time : %m%d%Y - %H:%M:%S");
+}
+
+void handleRoot() {
+  server.send(200, "text/plain", "ESP32 Dev module");
+}
+
+void handleJson() {
+  float pzem_voltage = pzem.voltage();
+  float pzem_current = pzem.current();
+  float pzem_power = pzem.power();
+  float pzem_energy = pzem.energy();
+  float pzem_frequency = pzem.frequency();
+  float pzem_pf = pzem.pf();
+  String jsonString = "{";
+  struct tm timeinfo;
+
+  if(getLocalTime(&timeinfo)){
+    char timeStringBuff[50];
+    jsonString += "\"date:\"";
+    strftime(timeStringBuff, sizeof(timeStringBuff), "%m%d%Y - %H:%M:%S", &timeinfo);
+    jsonString += timeStringBuff;
+  }
+
+  //TODO : tester le sprintf
+/*
+  char values[100];
+  sprintf(values, ",\"v:\"%s", pzem_voltage);
+*/
+  jsonString += ",\"v:\""; //Volts
+  jsonString += pzem_voltage;
+  jsonString += ",\"a:\""; //Ampere
+  jsonString += pzem_current;
+  jsonString += ",\"w:\""; //Watt
+  jsonString += pzem_power;
+  jsonString += ",\"kwh:\""; //kWh
+  jsonString += pzem_energy;
+  jsonString += ",\"hz:\""; //Hz
+  jsonString += pzem_frequency;
+  jsonString += ",\"pf:\""; //Power Factor
+  jsonString += pzem_pf;
+  jsonString += "}\n";
+
+  server.setContentLength(jsonString.length());
+  server.send(200, "application/json", jsonString);
+}
+
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  printLocalTime();
+  Serial.println(message);
+}
+
+void WiFiStationConnected(WiFiEvent_t event, WiFiEventInfo_t info){
+  Serial.println("Connected to AP successfully!");
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer); //init and get the time
+  printLocalTime();
+}
+
+void WiFiGotIP(WiFiEvent_t event, WiFiEventInfo_t info){
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void WiFiStationDisconnected(WiFiEvent_t event, WiFiEventInfo_t info){
+  printLocalTime();
+  Serial.print("WiFi lost connection. Reason: ");
+  Serial.println(info.disconnected.reason);
+  Serial.println("Trying to Reconnect");
+  WiFi.begin(ssid, password);
+}
+
 void setup() {
-    Serial.begin(115200);
-
-/* Setup WiFi Connexion */
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-    }
-    Serial.println("");
-    Serial.print("WiFi connected ! IP address: ");
-    Serial.println(WiFi.localIP());
-
-    server.begin();
-/* End WiFi Setup */
+  Serial.begin(115200);
+  WiFi.disconnect(true); // delete old config
+  delay(1000);
+  WiFi.onEvent(WiFiStationConnected, SYSTEM_EVENT_STA_CONNECTED);
+  WiFi.onEvent(WiFiGotIP, SYSTEM_EVENT_STA_GOT_IP);
+  WiFi.onEvent(WiFiStationDisconnected, SYSTEM_EVENT_STA_DISCONNECTED);
+  WiFi.begin(ssid, password);
+  server.on("/", handleRoot);
+  server.on("/output.json", handleJson);
+  server.onNotFound(handleNotFound);
 }
 
 void loop() {
-    WiFiClient client = server.available();   // listen for incoming clients
-    float pzem_voltage = pzem.voltage();
-    float pzem_current = pzem.current();
-    float pzem_power = pzem.power();
-    float pzem_energy = pzem.energy();
-    float pzem_frequency = pzem.frequency();
-    float pzem_pf = pzem.pf();
-
-    if (client) {                             // if you get a client,
-      String jsonString = "{";                // make a String to hold incoming data from the client
-      while (client.connected()) {            // loop while the client's connected
-        if (client.available()) {             // if there's bytes to read from the client,
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-Type: application/json");
-            client.println();
-
-            jsonString += "\"v:\""; //Volts
-            jsonString += pzem_voltage;
-            jsonString += ",\"a:\""; //Ampere
-            jsonString += pzem_current;
-            jsonString += ",\"w:\""; //Watt
-            jsonString += pzem_power;
-            jsonString += ",\"kwh:\""; //kWh
-            jsonString += pzem_energy;
-            jsonString += ",\"hz:\""; //Hz
-            jsonString += pzem_frequency;
-            jsonString += ",\"pf:\""; //Power Factor
-            jsonString += pzem_pf;
-
-            client.println(); // The HTTP response ends with another blank line:
-            break; // break out of the while loop:
-        }
-      }
-      jsonString += "}\n";
-      client.println(jsonString); //print jsonString sur port http
-      Serial.println(jsonString); //print jsonString sur port série
-      client.stop(); // close the connection:
-    }
-
-    delay(wait);
+  server.handleClient();
+  delay(wait); //allow the cpu to switch to other tasks
 }
