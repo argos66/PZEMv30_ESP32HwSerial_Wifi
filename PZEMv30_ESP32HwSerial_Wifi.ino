34ca2424156ced3ed5bb44729272463bf700c253
argos66 <argos66@users.noreply.github.com>
2021-06-01 07:26:44 +0200

    Update and rename PZEMv30_ESP32HwSerial_Wifi.ino.ino to PZEMv30_ESP32HwSerial_Wifi.ino

--- PZEMv30_ESP32HwSerial_Wifi.ino
+++ PZEMv30_ESP32HwSerial_Wifi.ino
@@ -1,14 +1,14 @@
-#include <HardwareSerial.h>
-#include <PZEM004Tv30.h>
-#include <WiFi.h>
-#include <WebServer.h>
-#include "time.h"
+#include <HardwareSerial.h> // Serie
+#include <PZEM004Tv30.h> //PZEM004T
+#include <WiFi.h> // Wifi
+#include <WebServer.h> //Web Server
+#include "time.h" //NTP
 
 #define wait 2
+#define ONBOARD_LED  2
 
 const char* ssid     = ""; //Wifi SSID
 const char* password = ""; //Wifi password
-
 const char* ntpServer = "pool.ntp.org";
 const long  gmtOffset_sec = 3600;
 const int   daylightOffset_sec = 3600;
@@ -19,15 +19,12 @@ PZEM004Tv30 pzem(&PzemSerial2);
 
 void printLocalTime(){
   struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
-    Serial.println("Failed to obtain time");
-    return;
-  }
-  Serial.println(&timeinfo, "NTP Time : %m%d%Y - %H:%M:%S");
+  if(!getLocalTime(&timeinfo)) Serial.println("Failed to obtain time");
+  Serial.println(&timeinfo, "NTP Time : %d-%M-%Y %H:%M:%S");
 }
 
 void handleRoot() {
-  server.send(200, "text/plain", "ESP32 Dev module");
+  server.send(200, "text/html", "\n<html>\n<body>\nESP32 Dev module <br /> Get result on : <a href=\"/output.json\">output.json</a>\n</body>\n</html>\n");
 }
 
 void handleJson() {
@@ -37,70 +34,47 @@ void handleJson() {
   float pzem_energy = pzem.energy();
   float pzem_frequency = pzem.frequency();
   float pzem_pf = pzem.pf();
-  String jsonString = "{";
   struct tm timeinfo;
+  String jsonString = "{";
 
   if(getLocalTime(&timeinfo)){
-    char timeStringBuff[50];
-    jsonString += "\"date:\"";
-    strftime(timeStringBuff, sizeof(timeStringBuff), "%m%d%Y - %H:%M:%S", &timeinfo);
-    jsonString += timeStringBuff;
+    char timeStringBuff[30];
+    jsonString += "\"date\":\"";
+    strftime(timeStringBuff, sizeof(timeStringBuff), "%d-%M-%Y %H:%M:%S", &timeinfo);
+    jsonString += String(timeStringBuff) + "\",";
   }
 
-  //TODO : tester le sprintf
-/*
-  char values[100];
-  sprintf(values, ",\"v:\"%s", pzem_voltage);
-*/
-  jsonString += ",\"v:\""; //Volts
-  jsonString += pzem_voltage;
-  jsonString += ",\"a:\""; //Ampere
-  jsonString += pzem_current;
-  jsonString += ",\"w:\""; //Watt
-  jsonString += pzem_power;
-  jsonString += ",\"kwh:\""; //kWh
-  jsonString += pzem_energy;
-  jsonString += ",\"hz:\""; //Hz
-  jsonString += pzem_frequency;
-  jsonString += ",\"pf:\""; //Power Factor
-  jsonString += pzem_pf;
-  jsonString += "}\n";
+  jsonString += "\"v\":" + String(pzem_voltage) + ",\"a\":" + String(pzem_current) + ",\"w\":" + String(pzem_power) + ",\"kwh\":" + String(pzem_energy) + ",\"hz\":" + String(pzem_frequency) + ",\"pf\":" + String(pzem_pf) + "}\n";
 
   server.setContentLength(jsonString.length());
   server.send(200, "application/json", jsonString);
 }
 
 void handleNotFound() {
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-  printLocalTime();
+  struct tm timeinfo;
+  char timeStringBuff[30];
+
+  if(getLocalTime(&timeinfo)) strftime(timeStringBuff, sizeof(timeStringBuff), "%d-%M-%Y %H:%M:%S", &timeinfo);
+  String message = String(timeStringBuff) + " - File Not Found. ";
+  message += "URI: " + server.uri();
+  server.send(404, "text/html", message);
   Serial.println(message);
 }
 
 void WiFiStationConnected(WiFiEvent_t event, WiFiEventInfo_t info){
   Serial.println("Connected to AP successfully!");
+}
+
+void WiFiGotIP(WiFiEvent_t event, WiFiEventInfo_t info){
+  digitalWrite(ONBOARD_LED,HIGH);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer); //init and get the time
   printLocalTime();
 }
 
-void WiFiGotIP(WiFiEvent_t event, WiFiEventInfo_t info){
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
 void WiFiStationDisconnected(WiFiEvent_t event, WiFiEventInfo_t info){
-  printLocalTime();
+  digitalWrite(ONBOARD_LED,LOW);
   Serial.print("WiFi lost connection. Reason: ");
   Serial.println(info.disconnected.reason);
   Serial.println("Trying to Reconnect");
@@ -108,16 +82,21 @@ void WiFiStationDisconnected(WiFiEvent_t event, WiFiEventInfo_t info){
 }
 
 void setup() {
+  pinMode(ONBOARD_LED,OUTPUT);
+  digitalWrite(ONBOARD_LED,LOW);
   Serial.begin(115200);
   WiFi.disconnect(true); // delete old config
-  delay(1000);
+  delay(500);
   WiFi.onEvent(WiFiStationConnected, SYSTEM_EVENT_STA_CONNECTED);
   WiFi.onEvent(WiFiGotIP, SYSTEM_EVENT_STA_GOT_IP);
   WiFi.onEvent(WiFiStationDisconnected, SYSTEM_EVENT_STA_DISCONNECTED);
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   server.on("/", handleRoot);
   server.on("/output.json", handleJson);
   server.onNotFound(handleNotFound);
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop() {
